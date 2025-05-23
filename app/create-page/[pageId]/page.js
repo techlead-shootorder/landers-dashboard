fabc8fe2631a109b93e90f0f2eb542d0139a15cb
farazshootorder <faraz@shootorder.in>
2025-05-23 11:55:33 +0530

    create page

--- app/create-page/[pageId]/page.js
+++ app/create-page/[pageId]/page.js
@@ -169,6 +169,7 @@ const page = () => {
                     <ThankYou
                         formData={formData}
                         goToPrevStep={goToPrevStep}
+                        pageId={pageId}
                     />
                 );
             default:
